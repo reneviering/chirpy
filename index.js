3de63d98a1d92d7533891a28cff165b350364e08
René Viering <rene.viering@gmail.com>
2015-11-14 19:30:45 +0100

    Serve static files.

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 5000 });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, reply) => {
-        reply('Hello, chirpy!');
+
+
+server.register(require('inert'), function (err) {
+	if (err) {
+        throw err;
     }
+    
+	server.route(require('./routes/index'));
 });
 
 server.start((err) => {
